1c82868fbaca05f3e463308cc48a2b5f8dec70f2
xaoxuu <16400144+xaoxuu@users.noreply.github.com>
2024-10-11 10:14:21 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@codeup.aliyun.com:6333b695257dab51ddaa62e9/mirrors/SDWebImage.git", from: "5.10.0")
+        // .package(url: "git@codeup.aliyun.com:6333b695257dab51ddaa62e9/mirrors/SDWebImage.git", from: "5.10.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SDWebImageSwiftUI",
-            dependencies: ["SDWebImage"],
+            dependencies: [],
             path: "SDWebImageSwiftUI",
             sources: ["Classes"],
             resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
